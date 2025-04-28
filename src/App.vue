cbebfe53f6d5f019d103fa7ea3916622c7e57c71
Bhuvesh bansal <96925288+bhuvesh-bansal@users.noreply.github.com>
2025-04-28 13:01:53 +0530

    Add landing page and configure Vercel routing

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <h1>Welcome to MedDocs</h1>
+    <HelloWorld />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+});
+</script>
+
+<style>
+#app {
+  text-align: center;
+  padding: 20px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Hello World!</h1>
+  <p>Welcome to the MedDocs application.</p>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HelloWorld',
+});
+</script>
+
+<style scoped>
+h1 {
+  color: #2c6ebb;
+}
+p {
+  font-size: 1.2em;
+}
+</style>
